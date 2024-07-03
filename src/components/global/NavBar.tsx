2584d41366abce8106273822f602722776f362f5
Kainy <iartkhay@gmail.com>
2024-07-03 02:38:41 +0100

    finish dropdown

--- src/components/global/NavBar.tsx
+++ src/components/global/NavBar.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import Link from 'next/link';
-import React, { useEffect, useRef, useState } from 'react'
-import Image from 'next/image';
+import React, { ReactNode, useEffect, useRef, useState } from 'react'
+import Image, { StaticImageData } from 'next/image';
 import logo from '@images/fumiwo-logo.png'
 import logoColor from '@images/fmw-logo-color.png'
 import dropdownImage from '@images/dropdown-image.png'
@@ -10,6 +10,7 @@ import { PrimaryButton } from './Buttons';
 import { useRouter, usePathname } from 'next/navigation';
 import { SideBar } from './SideBar';
 import { H5, P } from './Typography';
+import Divider from './Divider';
 
 
 // Dropdown.js
@@ -89,46 +90,81 @@ const Dropdown = () => {
 type TNav = {
 	title: string;
 	link: string;
+	description: string;
+	image: StaticImageData | ReactNode;
 }
 
 export type TNavLinks = TNav & {
-	dropDownLink?: TNav[];
+	dropDownLink?: TNav[]
 }
 
 export const navLinks: TNavLinks[] = [
 	{
 		title: 'Solutions',
 		link: '/solutions',
+		description: 'Explore our range of powerful solutions that resonate with your various business requirements.',
+		image: dropdownImage,
 		dropDownLink: [
 			{
-				title: 'Smart data',
-				link: '/solutions/smart-data',
+				title: 'Credit scoring',
+				link: '/solutions/credit-risk-score',
+				description: 'Make better lending decisions with credit scoring',
+				image: <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1.3 13C0.582029 13 -0.00673294 12.4165 0.0649452 11.7022C0.191566 10.4402 0.502426 9.20118 0.989567 8.02512C1.64288 6.44788 2.60045 5.01477 3.80761 3.80761C5.01477 2.60045 6.44788 1.64288 8.02512 0.989566C9.60235 0.336255 11.2928 -7.46234e-08 13 0C14.7072 7.46234e-08 16.3977 0.336255 17.9749 0.989566C19.5521 1.64288 20.9852 2.60045 22.1924 3.80761C23.3996 5.01477 24.3571 6.44788 25.0104 8.02512C25.4976 9.20118 25.8084 10.4402 25.9351 11.7022C26.0067 12.4165 25.418 13 24.7 13C23.982 13 23.4084 12.4157 23.3189 11.7034C23.2032 10.7831 22.9648 9.88056 22.6083 9.02009C22.0857 7.75831 21.3196 6.61182 20.3539 5.64609C19.3882 4.68036 18.2417 3.9143 16.9799 3.39165C15.7181 2.869 14.3657 2.6 13 2.6C11.6343 2.6 10.2819 2.869 9.02009 3.39165C7.75831 3.9143 6.61182 4.68036 5.64609 5.64609C4.68036 6.61182 3.9143 7.75831 3.39165 9.02009C3.03524 9.88055 2.79678 10.7831 2.68114 11.7034C2.59163 12.4157 2.01797 13 1.3 13Z" fill="currentColor" />
+					<circle cx="11.3241" cy="17.8202" r="3.44125" fill="currentColor" />
+					<path d="M19.0494 10.24L11.509 20.5526L8.29751 17.1396L19.0494 10.24Z" fill="currentColor" />
+				</svg>
+
 			},
-			// {
-			// 	title: 'Credit risk score',
-			// 	link: '/solutions/credit-risk-score',
-			// },
 			{
-				title: 'Fraud insights',
+				title: 'Fraud',
 				link: '/solutions/fraud-insights',
+				description: 'Identify potentially fraudulent borrowers with our solution',
+				image: <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M23.8281 8.71632C23.8281 16.8515 19.0556 22.4737 12.6953 24.0953C6.33499 22.4737 1.5625 16.8515 1.5625 8.71632C1.5625 7.0269 1.76832 5.39531 2.15144 3.85587C3.25813 4.62693 4.60361 5.07906 6.05469 5.07906C9.26867 5.07906 10.9254 0.404297 12.6953 0.404297C14.5754 0.404297 16.122 5.07906 19.3359 5.07906C20.787 5.07906 22.1325 4.62693 23.2392 3.85587C23.6223 5.39531 23.8281 7.0269 23.8281 8.71632Z" fill="#080341" />
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M10.5469 14.5767C9.37101 13.465 8.59375 11.6647 8.59375 10.1563C8.59375 7.78315 10.5175 5.85938 12.8906 5.85938C15.2637 5.85938 17.1875 7.78315 17.1875 10.1563C17.1875 11.6647 16.4102 13.465 15.2344 14.5767V17.1875C15.2344 17.1875 13.6455 17.7534 12.8906 17.7534C12.1357 17.7534 10.5469 17.1875 10.5469 17.1875V14.5767Z" fill="white" />
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M9.76562 16.7969L12.8898 17.5305L9.76562 19.1406V16.7969Z" fill="white" />
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M16.0473 16.7969L12.8906 17.5199L16.0473 19.1406" fill="white" />
+					<path d="M9.375 13.2812C10.0222 13.2812 10.5469 12.7566 10.5469 12.1094C10.5469 11.4622 10.0222 10.9375 9.375 10.9375C8.72779 10.9375 8.20312 11.4622 8.20312 12.1094C8.20312 12.7566 8.72779 13.2812 9.375 13.2812Z" fill="white" />
+					<path d="M16.4062 13.2812C17.0535 13.2812 17.5781 12.7566 17.5781 12.1094C17.5781 11.4622 17.0535 10.9375 16.4062 10.9375C15.759 10.9375 15.2344 11.4622 15.2344 12.1094C15.2344 12.7566 15.759 13.2812 16.4062 13.2812Z" fill="white" />
+				</svg>
 			}
 		]
 	},
 	{
 		title: 'Company',
-		link: '/about-us'
+		link: '/about-us',
+		description: '',
+		image: dropdownImage
 	},
 	{
 		title: 'Resources',
 		link: '#',
+		description: 'Explore our range of powerful solutions that resonate with your various business requirements.',
+		image: dropdownImage,
 		dropDownLink: [
 			{
 				title: 'Blog',
-				link: '#'
+				link: '#',
+				description: 'Stay up to date with latest articles from Fumiwo',
+				image: <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1.3 13C0.582029 13 -0.00673294 12.4165 0.0649452 11.7022C0.191566 10.4402 0.502426 9.20118 0.989567 8.02512C1.64288 6.44788 2.60045 5.01477 3.80761 3.80761C5.01477 2.60045 6.44788 1.64288 8.02512 0.989566C9.60235 0.336255 11.2928 -7.46234e-08 13 0C14.7072 7.46234e-08 16.3977 0.336255 17.9749 0.989566C19.5521 1.64288 20.9852 2.60045 22.1924 3.80761C23.3996 5.01477 24.3571 6.44788 25.0104 8.02512C25.4976 9.20118 25.8084 10.4402 25.9351 11.7022C26.0067 12.4165 25.418 13 24.7 13C23.982 13 23.4084 12.4157 23.3189 11.7034C23.2032 10.7831 22.9648 9.88056 22.6083 9.02009C22.0857 7.75831 21.3196 6.61182 20.3539 5.64609C19.3882 4.68036 18.2417 3.9143 16.9799 3.39165C15.7181 2.869 14.3657 2.6 13 2.6C11.6343 2.6 10.2819 2.869 9.02009 3.39165C7.75831 3.9143 6.61182 4.68036 5.64609 5.64609C4.68036 6.61182 3.9143 7.75831 3.39165 9.02009C3.03524 9.88055 2.79678 10.7831 2.68114 11.7034C2.59163 12.4157 2.01797 13 1.3 13Z" fill="#011456" />
+					<circle cx="11.3241" cy="17.8202" r="3.44125" fill="#011456" />
+					<path d="M19.0494 10.24L11.509 20.5526L8.29751 17.1396L19.0494 10.24Z" fill="#011456" />
+				</svg>
 			},
 			{
 				title: 'Developers',
-				link: '#'
+				link: '#',
+				description: 'Explore our easy and concise developer documentations',
+				image: <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M23.8281 8.71632C23.8281 16.8515 19.0556 22.4737 12.6953 24.0953C6.33499 22.4737 1.5625 16.8515 1.5625 8.71632C1.5625 7.0269 1.76832 5.39531 2.15144 3.85587C3.25813 4.62693 4.60361 5.07906 6.05469 5.07906C9.26867 5.07906 10.9254 0.404297 12.6953 0.404297C14.5754 0.404297 16.122 5.07906 19.3359 5.07906C20.787 5.07906 22.1325 4.62693 23.2392 3.85587C23.6223 5.39531 23.8281 7.0269 23.8281 8.71632Z" fill="#080341" />
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M10.5469 14.5767C9.37101 13.465 8.59375 11.6647 8.59375 10.1563C8.59375 7.78315 10.5175 5.85938 12.8906 5.85938C15.2637 5.85938 17.1875 7.78315 17.1875 10.1563C17.1875 11.6647 16.4102 13.465 15.2344 14.5767V17.1875C15.2344 17.1875 13.6455 17.7534 12.8906 17.7534C12.1357 17.7534 10.5469 17.1875 10.5469 17.1875V14.5767Z" fill="white" />
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M9.76562 16.7969L12.8898 17.5305L9.76562 19.1406V16.7969Z" fill="white" />
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M16.0473 16.7969L12.8906 17.5199L16.0473 19.1406" fill="white" />
+					<path d="M9.375 13.2812C10.0222 13.2812 10.5469 12.7566 10.5469 12.1094C10.5469 11.4622 10.0222 10.9375 9.375 10.9375C8.72779 10.9375 8.20312 11.4622 8.20312 12.1094C8.20312 12.7566 8.72779 13.2812 9.375 13.2812Z" fill="white" />
+					<path d="M16.4062 13.2812C17.0535 13.2812 17.5781 12.7566 17.5781 12.1094C17.5781 11.4622 17.0535 10.9375 16.4062 10.9375C15.759 10.9375 15.2344 11.4622 15.2344 12.1094C15.2344 12.7566 15.759 13.2812 16.4062 13.2812Z" fill="white" />
+				</svg>
 			}
 		]
 	}
@@ -143,6 +179,7 @@ const NavBar = ({ dark }: { dark?: boolean }) => {
 
 	const toggleName = (name: string) => {
 		setToggleDropDownName(prev => prev === name ? '' : name)
+		// setToggleDropDownName(prev => prev === name ? '' : name)
 	}
 
 	const toggleSideBar = () => {
@@ -177,7 +214,7 @@ const NavBar = ({ dark }: { dark?: boolean }) => {
 			/>
 			<nav
 				onMouseOut={() => setToggleDropDownName('')}
-				className={` md:py-6 ${dark ? 'text-white bg-primaryBlack' : 'text-linkGray bg-white'}`}
+				className={`transition md:py-6 ${dark ? 'text-white bg-primaryBlack' : 'text-linkGray bg-white'}`}
 			>
 				<div onClick={e => e.stopPropagation()} className="container mx-auto ">
 					<div className="flex justify-between items-center p-4">
@@ -193,14 +230,14 @@ const NavBar = ({ dark }: { dark?: boolean }) => {
 						<ul className={`hidden md:flex space-x-8 ${dark ? 'text-linkGray ' : 'text-primaryBlack'}`}>
 							{navLinks.map((navLink, index) => (
 								<li onMouseOver={() => toggleName(navLink.title)} key={index} className='cursor-pointer relative'>
-									<div className={`space-x-2  ${pathname.includes(navLink.link) ? 'text-primaryGreen' : ''}`}>
+									<div className={`space-x-2  ${dark ? 'hover:text-white' : 'text-paraGray hover:text-black'} ${pathname.includes(navLink.link) ? '!text-primaryGreen' : ''}`}>
 										<Link href={navLink.dropDownLink ? '#' : navLink.link}>
 											{navLink.title}
 										</Link>
 										{navLink.dropDownLink && (
 											<button>
 												<svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-													<path d="M7.5 8.40625L13.5938 2.34375C13.875 2.03125 13.875 1.5625 13.5938 1.28125L12.875 0.5625C12.5938 0.28125 12.125 0.28125 11.8125 0.5625L7 5.375L2.15625 0.5625C1.84375 0.28125 1.375 0.28125 1.09375 0.5625L0.375 1.28125C0.09375 1.5625 0.09375 2.03125 0.375 2.34375L6.46875 8.40625C6.75 8.6875 7.21875 8.6875 7.5 8.40625Z" fill="#BAB7B7" />
+													<path d="M7.5 8.40625L13.5938 2.34375C13.875 2.03125 13.875 1.5625 13.5938 1.28125L12.875 0.5625C12.5938 0.28125 12.125 0.28125 11.8125 0.5625L7 5.375L2.15625 0.5625C1.84375 0.28125 1.375 0.28125 1.09375 0.5625L0.375 1.28125C0.09375 1.5625 0.09375 2.03125 0.375 2.34375L6.46875 8.40625C6.75 8.6875 7.21875 8.6875 7.5 8.40625Z" fill="currentColor" />
 												</svg>
 											</button>
 										)}
@@ -208,35 +245,35 @@ const NavBar = ({ dark }: { dark?: boolean }) => {
 
 
 									{navLink.dropDownLink && (
-										<div className='-left-1/2 pt-5 absolute min-w-[710px] top-6 '>
-											<div className={`flex rounded-md bg-white shadow-optionShadow py-8 px-10 ${toggleDropDownName === navLink.title ? 'block' : 'hidden'}`}>
-												<section className='space-y-12 flex-1'>
-													<H5>
-														Solutions
+										<div className='-left-1/2 -translate-x-1/3 pt-5 absolute  top-6 '>
+											<div className={`flex gap-6 rounded-md min-w-[810px] bg-white shadow-optionShadow py-8 px-10 cursor-default ${toggleDropDownName === navLink.title ? 'block' : 'hidden'}`}>
+												<section className=' flex-1'>
+													<H5 className='!text-[28px]'>
+														{navLink.title}
 													</H5>
-													<P>
+													<Divider height={4} />
+													<P className='!text-base'>
 														Explore Fumiwo’s range of powerful end-to-end solutions that resonate with your various business requirements.
 													</P>
-
+													<Divider height={18} />
 													<Image
-														src={dropdownImage}
+														src={navLink.image as StaticImageData}
 														alt='dropdown image'
-														className='w-[259px] h-[204px]'
+														className='w-[164px] h-[129px]'
 													/>
 												</section>
-												<section className='flex-1'>
+												<section className='flex-1 space-y-7'>
 													{navLink.dropDownLink.map((item, index) => (
-														<div key={index} className='flex flex-col md:flex-row gap-6'>
-															<div>
-																<svg width="45" height="44" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-																	<path fill-rule="evenodd" clip-rule="evenodd" d="M15.7367 10.8402C14.1482 11.6344 13.6869 12.4784 13.6869 13.0036C13.6869 13.5288 14.1482 14.3727 15.7367 15.167C17.2433 15.9203 19.4204 16.4257 21.9 16.4257C24.3796 16.4257 26.5567 15.9203 28.0633 15.167C29.6517 14.3727 30.1131 13.5288 30.1131 13.0036C30.1131 12.4784 29.6517 11.6344 28.0633 10.8402C26.5567 10.0869 24.3796 9.58145 21.9 9.58145C19.4204 9.58145 17.2433 10.0869 15.7367 10.8402ZM30.1131 17.1529C29.8457 17.3207 29.5689 17.475 29.2876 17.6157C27.3263 18.5963 24.7124 19.1634 21.9 19.1634C19.0877 19.1634 16.4738 18.5963 14.5124 17.6157C14.2312 17.475 13.9544 17.3207 13.6869 17.1529V18.9353C13.6869 19.5567 14.1937 20.4514 15.7597 21.2718C17.2609 22.0581 19.4295 22.5855 21.9 22.5855C24.3705 22.5855 26.5391 22.0581 28.0403 21.2718C29.6063 20.4514 30.1131 19.5567 30.1131 18.9353V17.1529ZM32.8508 13.0036V18.9353C32.8508 19.0119 32.8488 19.088 32.8446 19.1634H32.8508V24.4107C32.8508 24.4873 32.8488 24.5634 32.8446 24.6388H32.8508V29.8861C32.8508 32.0364 31.2126 33.6515 29.3106 34.6476C27.3438 35.6779 24.7215 36.274 21.9 36.274C19.0785 36.274 16.4562 35.6779 14.4894 34.6476C12.5874 33.6515 10.9492 32.0364 10.9492 29.8861V24.6388H10.9554C10.9513 24.5634 10.9492 24.4873 10.9492 24.4107V19.1634H10.9554C10.9513 19.088 10.9492 19.0119 10.9492 18.9353V13.0036C10.9492 10.8828 12.6329 9.33126 14.5124 8.3915C16.4738 7.41082 19.0877 6.84375 21.9 6.84375C24.7124 6.84375 27.3263 7.41082 29.2876 8.3915C31.1671 9.33126 32.8508 10.8828 32.8508 13.0036ZM13.6869 28.7028V29.8861C13.6869 30.5075 14.1937 31.4022 15.7597 32.2226C17.2609 33.0089 19.4295 33.5363 21.9 33.5363C24.3705 33.5363 26.5391 33.0089 28.0403 32.2226C29.6063 31.4022 30.1131 30.5075 30.1131 29.8861V28.7028C29.8539 28.872 29.5849 29.0286 29.3106 29.1722C27.3438 30.2025 24.7215 30.7986 21.9 30.7986C19.0785 30.7986 16.4562 30.2025 14.4894 29.1722C14.215 29.0286 13.9462 28.872 13.6869 28.7028ZM29.3106 23.6968C29.5849 23.5532 29.8539 23.3966 30.1131 23.2274V24.4107C30.1131 25.0321 29.6063 25.9268 28.0403 26.7472C26.5391 27.5335 24.3705 28.0609 21.9 28.0609C19.4295 28.0609 17.2609 27.5335 15.7597 26.7472C14.1937 25.9268 13.6869 25.0321 13.6869 24.4107V23.2274C13.9462 23.3966 14.215 23.5532 14.4894 23.6968C16.4562 24.7271 19.0785 25.3232 21.9 25.3232C24.7215 25.3232 27.3438 24.7271 29.3106 23.6968Z" fill="#080341" />
-																</svg>
-
-															</div>
-															<div className='space-y-1'>
-																<h6 className='font-semibold text-2xl leading-[48px] text-textHeader'>{item.title}</h6>
-																<P className=''>Explore Fumiwo’s range of powerful end-to-end products that resonate with your various business requirements.</P>
-															</div>
+														<div key={index} className=''>
+															<Link key={index} href={item.link} className='flex flex-col md:flex-row gap-6 text-textHeader hover:text-primaryGreen duration-200'>
+																<div className='mt-1 '>
+																	{item.image as ReactNode}
+																</div>
+																<div className='space-y-3 '>
+																	<h6 className='font-semibold text-2xl    '>{item.title}</h6>
+																	<P className='!text-base '>{item.description}</P>
+																</div>
+															</Link>
 														</div>
 													))}
 
@@ -246,7 +283,7 @@ const NavBar = ({ dark }: { dark?: boolean }) => {
 											</div>
 
 										</div>
-								)}
+									)}
 
 								</li>
 							))}
